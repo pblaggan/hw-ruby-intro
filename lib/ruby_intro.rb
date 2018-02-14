ef03ee170d962e02ba1c19f4c196f85a38ffd3af
pblaggan <pblaggan@tamu.edu>
2018-02-13 23:14:27 -0500

    try1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,35 +1,70 @@
-# When done, submit this entire file to the autograder.
+# Arrays, Hashes and Enumerables
+# 1.0
 
-# Part 1
-
-def sum arr
-  # YOUR CODE HERE
+def sum elements
+  elements.reduce 0, :+
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+# 1.1
+
+def max_2_sum elements
+  sum(elements.sort.last(2))
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+# 1.2
+
+def sum_to_n? elements, total
+  if elements.empty?
+    return false if total == 0
+  else
+    elements.combination(2).to_a.each do |pair|
+      return true if sum(pair) == total
+    end
+  end
+
+  return false
 end
 
-# Part 2
 
-def hello(name)
-  # YOUR CODE HERE
+
+# Strings and Regular Expressions
+# 2.0
+
+def hello name
+  "Hello, " + name
 end
+
+# 2.1
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  !!(s[0] =~ /[bcdfghjklmnprstvwxyz]+/i)
 end
+
+# 2.2
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~ /^[0-1]+$/
+    return s.to_i(2) % 4 == 0 
+  end
+  false
 end
 
-# Part 3
+
+
+# Object Oriented Basics
 
 class BookInStock
-# YOUR CODE HERE
+
+  attr_accessor :isbn, :price
+
+  def initialize isbn, price
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format("$%.2f", @price)
+  end
+
 end
